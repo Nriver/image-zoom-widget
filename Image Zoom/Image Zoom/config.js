d51fe9721d738715d8a518c63f1151e4adedde50
Nriver <6752679+Nriver@users.noreply.github.com>
2025-01-10 14:13:36 +0800

    change default zoom factor

--- Image Zoom/Image Zoom/config.js
+++ Image Zoom/Image Zoom/config.js
@@ -1,9 +1,9 @@
 // If it doesn’t work in the browser, try increasing the delay to better suit your settings.
 var executeDelay = 200;
 
-var zoomFactor = 1.3;    // Zoom factor for mouse wheel, large factor results in faster zoom
-var minZoomScale = 0.1;  // Minimum zoom scale
-var maxZoomScale = 10;    // Maximum zoom scale
+var zoomFactor = 1.1;    // Zoom factor for mouse wheel, large factor results in faster zoom
+var minZoomScale = 0.2;  // Minimum zoom scale
+var maxZoomScale = 5;    // Maximum zoom scale
 
 module.exports = {
     executeDelay,
